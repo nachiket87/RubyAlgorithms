74c6e198fd8632a333e84d2531e2130c7a439c18
Nachiket Pusalkar <nachiket87@gmail.com>
2021-01-03 16:17:32 -0500

    reverseing words :fire:

---
+++ 03-ReverseWords/main.rb
@@ -0,0 +1,43 @@
+=begin
+def reverse_words(string)
+  string.split.reverse.join(" ") # easy way.
+end
+=end
+
+def reverse_words!(message)
+
+  #first we reverse the entire thing
+  reverse_characters!(message, 0, message.length - 1)
+
+  current_start = 0
+
+  #then we reverse each word
+  (0..message.length).each do |i|
+    # keep going unless message is over or we found a space
+    next unless i == message.length || message[i] == ' '
+
+    # if we found a space reverse the word 
+
+    reverse_characters!(message, current_start, i - 1)
+
+    #start the next word ... we already know that there can only be 1 space
+
+    current_start = i + 1
+  end
+
+  return message #don't need to return but doing this to test spec
+end
+
+def reverse_characters!(message, left_index, right_index)
+
+  # Walk towards the middle, from both sides.
+  while left_index < right_index
+
+    # Swap the left char and right char.
+    message[left_index], message[right_index] =
+      message[right_index], message[left_index]
+
+    left_index  += 1
+    right_index -= 1
+  end
+end
---
+++ 03-ReverseWords/spec/main_spec.rb
@@ -0,0 +1,24 @@
+require "./spec_helper.rb"
+require "../main.rb"
+
+
+describe "reverse_words" do
+
+  it "return a string" do 
+    expect(reverse_words!("string").class).to eq(String)
+  end
+
+  it "return a reversed string" do 
+    message = 'cake pound steal'
+    expected = 'steal pound cake' 
+    expect(reverse_words!(message)).to eq(expected)
+  end
+
+  it "return a reversed string test case 2" do 
+    expect(reverse_words!('nachiket is a good programmer')).to eq('programmer good a is nachiket')
+  end
+
+  it "return a reversed string test case 2" do 
+    expect(reverse_words!('i am a javascript developer')).to eq('developer javascript a am i')
+  end
+end
