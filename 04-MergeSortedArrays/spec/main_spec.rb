100dac0e4143d7c1e36e013bca07fdced7fd3ee6
Nachiket Pusalkar <nachiket87@gmail.com>
2021-01-04 00:14:49 -0500

    fixed and done :fire:

--- 04-MergeSortedArrays/spec/main_spec.rb
+++ 04-MergeSortedArrays/spec/main_spec.rb
@@ -1,18 +1,19 @@
-require './spec_helper.rb'
-require '../main.rb'
+# frozen_string_literal: true
+
+require './spec_helper'
+require '../main'
 
 describe 'merge_arrays' do
   it 'should return an array' do
-    expect(merge_arrays([1,2,3],[1,2,3,4]).class).to eq(Array)
+    expect(merge_arrays([1, 2, 3], [1, 2, 3, 4]).class).to eq(Array)
   end
 
   it 'should accept only 2 arguments' do
-    expect { merge_arrays([1,2,3],[1,2,3,4], 1) }.to raise_error(ArgumentError)
+    expect { merge_arrays([1, 2, 3], [1, 2, 3, 4], 1) }.to raise_error(ArgumentError)
   end
   it 'should return a sorted array' do
     array1 = [3, 4, 6, 10, 11, 15]
     array2 = [1, 5, 8, 12, 14, 19]
-    expect(merge_arrays(array1,array2)).to eq([1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19])
+    expect(merge_arrays(array1, array2)).to eq([1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19])
   end
-
 end
