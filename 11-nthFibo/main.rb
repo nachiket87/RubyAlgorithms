1f32773722c7b5c1bfe01f0aa793e19ec3eef24e
Nachiket Pusalkar <nachiket87@gmail.com>
2021-01-08 19:22:55 -0500

    fixed with memoize

--- 11-nthFibo/main.rb
+++ 11-nthFibo/main.rb
@@ -9,7 +9,8 @@ end
 # 1, 1, 2, 3, 5, 7, 12, 19 
 
 # let's try with a loop
-def fib(n)
+
+def fib_loops(n)
   return n unless n >= 2
   return 1 if n == 2
 
@@ -21,9 +22,31 @@ def fib(n)
     next_number = total # 2, 3
     n-=1;
   end
-
   total
 end
+
+# still not the best, perhaps memoize?
+
+
+class Fiboo
+  def initialize
+    @memo = {}
+  end
+
+  def fib(n)
+    return n if n < 2;
+
+    return @memo[n] if @memo.include?(n)
+
+    result = fib(n-2) + fib(n-1)
+    @memo[n] = result
+
+    return result
+
+  end
+end
+
+
 
 
 
--- 11-nthFibo/spec/main_spec.rb
+++ 11-nthFibo/spec/main_spec.rb
@@ -1,7 +1,37 @@
 require './spec_helper.rb'
 require '../main.rb'
 
-describe 'fib (n) should return nth fibonacci number' do
+describe Fiboo do
+  fiboo = Fiboo.new
+  it 'should return 0 for 0' do
+    expect(fiboo.fib(0)).to eq(0)
+  end
+
+  it 'should return 1 for 1' do
+    expect(fiboo.fib(1)).to eq(1)
+  end
+
+  it 'should return 1 for 2' do
+    expect(fiboo.fib(2)).to eq(1)
+  end
+
+  it 'should return 2 for 3' do
+    expect(fiboo.fib(3)).to eq(2)
+  end
+
+  it 'should return 3 for 4' do
+    expect(fiboo.fib(4)).to eq(3)
+  end
+
+end
+
+
+=begin 
+
+
+# Uncomment if you want to test just a method
+
+describe 'testing just a function recursion' do
   it 'should return 0 for 0' do
     expect(fib(0)).to eq(0)
   end
@@ -23,3 +53,7 @@ describe 'fib (n) should return nth fibonacci number' do
   end
 
 end
+
+
+=end
+
